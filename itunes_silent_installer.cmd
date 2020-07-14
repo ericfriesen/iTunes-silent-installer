efb69ed3dbb9641045873d5b43d03282154e0037
ericfriesen <25352077+ericfriesen@users.noreply.github.com>
2020-07-14 16:13:30 -0500

    Update itunes_silent_installer.cmd

--- itunes_silent_installer.cmd
+++ itunes_silent_installer.cmd
@@ -1,10 +1,10 @@
 cd /d %tmp%
-rd /s /q itunes_silent
+if exist itunes_silent rd /s /q itunes_silent
 md itunes_silent
 cd itunes_silent
 
-powershell.exe Invoke-WebRequest -Uri "http://stahlworks.com/dev/unzip.exe" -OutFile "unzip.exe"
-powershell.exe Invoke-WebRequest -Uri "https://www.7-zip.org/a/7za920.zip" -OutFile "7za920.zip"
+powershell.exe Start-BitsTransfer -Source "http://stahlworks.com/dev/unzip.exe" -Destination "unzip.exe"
+powershell.exe Start-BitsTransfer -Source "https://www.7-zip.org/a/7za920.zip" -Destination "7za920.zip"
 unzip.exe -o 7za920.zip 7za.exe
 
 reg.exe query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > nul && set os=32bit || set os=64bit
@@ -12,28 +12,28 @@ reg.exe query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x
 title %os%
 if %os%==64bit goto 64bits
 
-powershell.exe Invoke-WebRequest -Uri "https://www.apple.com/itunes/download/win32" -OutFile "itunes32.exe"
+powershell.exe Start-BitsTransfer -Source "https://www.apple.com/itunes/download/win32" -Destination "itunes32.exe"
 7za.exe e itunes32.exe *.msi
 
-msiexec.exe /i AppleApplicationSupport.msi /qn REBOOT=ReallySuppress
-msiexec.exe /i Bonjour.msi /qn REBOOT=ReallySuppress
-msiexec.exe /i AppleMobileDeviceSupport.msi /qn REBOOT=ReallySuppress
-msiexec.exe /i AppleSoftwareUpdate.msi /qn REBOOT=ReallySuppress
-msiexec.exe /i iTunes.msi /qn REBOOT=ReallySuppress
+rem msiexec.exe /i AppleApplicationSupport.msi /qn REBOOT=ReallySuppress
+rem msiexec.exe /i Bonjour.msi /qn REBOOT=ReallySuppress
+rem msiexec.exe /i AppleMobileDeviceSupport.msi /qn REBOOT=ReallySuppress
+rem msiexec.exe /i AppleSoftwareUpdate.msi /qn REBOOT=ReallySuppress
+rem msiexec.exe /i iTunes.msi /qn REBOOT=ReallySuppress
 
 goto finish
 
 :64bits
 
-powershell.exe Invoke-WebRequest -Uri "https://www.apple.com/itunes/download/win64" -OutFile "itunes64.exe"
+powershell.exe Start-BitsTransfer -Source "https://www.apple.com/itunes/download/win64" -Destination "itunes64.exe"
 7za.exe e itunes64.exe *.msi
 
-msiexec.exe /i AppleApplicationSupport.msi /qn REBOOT=ReallySuppress
-msiexec.exe /i AppleApplicationSupport64.msi /qn REBOOT=ReallySuppress
-msiexec.exe /i Bonjour64.msi /qn REBOOT=ReallySuppress
-msiexec.exe /i AppleMobileDeviceSupport64.msi /qn REBOOT=ReallySuppress
-msiexec.exe /i AppleSoftwareUpdate.msi /qn REBOOT=ReallySuppress
-msiexec.exe /i iTunes64.msi /qn REBOOT=ReallySuppress
+rem msiexec.exe /i AppleApplicationSupport.msi /qn REBOOT=ReallySuppress
+rem msiexec.exe /i AppleApplicationSupport64.msi /qn REBOOT=ReallySuppress
+rem msiexec.exe /i Bonjour64.msi /qn REBOOT=ReallySuppress
+rem msiexec.exe /i AppleMobileDeviceSupport64.msi /qn REBOOT=ReallySuppress
+rem msiexec.exe /i AppleSoftwareUpdate.msi /qn REBOOT=ReallySuppress
+rem msiexec.exe /i iTunes64.msi /qn REBOOT=ReallySuppress
 
 :finish
 
