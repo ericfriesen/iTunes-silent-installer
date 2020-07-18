07b3c45fdc7c8817724797bb732c5ead108f45fc
ericfriesen <25352077+ericfriesen@users.noreply.github.com>
2020-07-18 17:33:57 -0500

    Update itunes_silent.ps1
    cleanup

--- itunes_silent.ps1
+++ itunes_silent.ps1
@@ -10,19 +10,14 @@ $Destination = "$TempDir\itunes_silent"
 Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::ExtractToDirectory($7z, $Destination)
 [System.IntPtr]::Size
-if ((gwmi win32_operatingsystem | select osarchitecture).osarchitecture -eq "64-bit")
-{
-	$WebClient.DownloadFile("https://www.apple.com/itunes/download/win64","$TempDir\itunes_silent\itunes_setup.exe")
-}
-else
-{
-	$WebClient.DownloadFile("https://www.apple.com/itunes/download/win32","$TempDir\itunes_silent\itunes_setup.exe")
-}
+if ((gwmi win32_operatingsystem | select osarchitecture).osarchitecture -eq "64-bit") {
+	$WebClient.DownloadFile("https://www.apple.com/itunes/download/win64","$TempDir\itunes_silent\itunes_setup.exe") }
+else {
+	$WebClient.DownloadFile("https://www.apple.com/itunes/download/win32","$TempDir\itunes_silent\itunes_setup.exe") }
 Start-Process -Wait -FilePath ".\7za.exe" -ArgumentList "e itunes_setup.exe *.msi" -NoNewWindow
 $msiFiles = Get-ChildItem -Path "$Destination" -Recurse -Include *.msi
 foreach ( $file in $msiFiles ) {
 	$fullPath = $file.FullName
-	Start-Process -Wait -FilePath "msiexec.exe" -ArgumentList  "/I `"$fullPath`"","/qn","REBOOT=ReallySuppress"
-}
-cd..
+	Start-Process -Wait -FilePath "msiexec.exe" -ArgumentList  "/I `"$fullPath`"","/qn","REBOOT=ReallySuppress" }
+cd $TempDir
 Remove-Item $TempDir\itunes_silent -Recurse -Force
